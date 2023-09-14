dadc547652950b676e55df02b15ebf00710f714e
jelenqq <petroviccc.y@gmail.com>
2023-09-14 17:35:29 +0200

    Initial commit

---
+++ src/PrvaKlasa.java
@@ -0,0 +1,13 @@
+
+public class PrvaKlasa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//prvi komentar
+		
+		/*viselinijski 
+		 * komentar
+		 */
+	}
+
+}
